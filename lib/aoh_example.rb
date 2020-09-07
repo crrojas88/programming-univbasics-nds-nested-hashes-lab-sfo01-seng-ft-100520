6039c6906b49d7d8de26f13ed96112d7c3b93388
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-07 05:38:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,8 +19,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  
   array = [
     {:name => "Don Gately", :occupation => "Live-in Staff"},
     {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
